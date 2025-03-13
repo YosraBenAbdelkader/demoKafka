abbb5363e15722a375cb3f9e857405ceec09bb01
Yosra <yosra_benabdelkader@carrefour.com>
2025-03-14 00:55:58 +0100

    Initial commit

---
+++ src/main/java/com/kafka/demoKafka/service/Consumer.java
@@ -0,0 +1,14 @@
+package com.kafka.demoKafka.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+
+    @KafkaListener(topics = "codeDecode_Topic",groupId = "codeDecode_Group")
+    public void listenToTopic (String recievedMsg)
+    {
+        System.out.println("the msg recieved is "+ recievedMsg);
+    }
+}
